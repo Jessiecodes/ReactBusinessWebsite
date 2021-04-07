7e0b09693ff076910a6a917f25fb2bbb5d5d4b6f
Jessica Leverett <Jessak1202@gmail.com>
2021-04-07 10:21:52 -0500

    first commit

---
+++ src/components/pages/HomePage/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import HeroSection from '../../HeroSection'
+import {homeObjFour, homeObjOne, homeObjThree, homeObjTwo} from './Data'
+
+
+function Home() {
+    return (
+        <>
+         <HeroSection {...homeObjOne} />
+         <HeroSection {...homeObjTwo} />
+         <HeroSection {...homeObjThree} />
+         <HeroSection {...homeObjFour} />
+        </>
+    )
+}
+
+export default Home
